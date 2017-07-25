f30c0922b3cf1f98b3d8790eca2a62cf14f742c6
Mikhail <realsonic3@gmail.com>
2017-07-25 14:36:52 +0300

    Добавьте файлы проекта.

---
+++ IFluent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFluent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
