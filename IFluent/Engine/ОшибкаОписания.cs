d76c6b2b8c672c6bc20fc35066901b1c1d7cf84a
Mikhail <realsonic3@gmail.com>
2017-07-25 15:15:59 +0300

    Вынес классы движка в отдельные файлы в папку Engine; заменил srting на новый неявноконвертирумый класс Текст

---
+++ IFluent/Engine/ОшибкаОписания.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IFluent.Engine
+{
+    internal class ОшибкаОписания : Exception
+    {
+        public ОшибкаОписания(Текст сообщение) : base(сообщение) { }
+    }
+}
---
+++ IFluent/Engine/Текст.cs
@@ -0,0 +1,27 @@
+﻿namespace IFluent.Engine
+{
+    internal class Текст
+    {
+        private readonly string _string;
+
+        private Текст(string @string)
+        {
+            _string = @string;
+        }
+
+        public static implicit operator string(Текст текст)
+        {
+            return текст._string;
+        }
+
+        public static implicit operator Текст(string @string)
+        {
+            return new Текст(@string);
+        }
+
+        public override string ToString()
+        {
+            return _string;
+        }
+    }
+}
