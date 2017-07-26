0a25e0c2e69f36b70e772c8a738543c193007ba3
Mikhail <realsonic3@gmail.com>
2017-07-26 13:27:22 +0300

    Перевод проекта на Framework 3.5; extension-метод Join для IEnumerable<string>

---
+++ IFluent/Движок/EnumerableExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IFluent.Движок
+{
+    public static class EnumerableExt
+    {
+        public static string Join(this IEnumerable<string> enumerable, string separator = "")
+        {
+            return string.Join(separator, enumerable.ToArray());
+        }
+    }
+}
