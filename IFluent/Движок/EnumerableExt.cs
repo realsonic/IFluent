59a30d7afe0f7f5ad40a6728a03d3637d9a98cd7
Mikhail <realsonic3@gmail.com>
2017-07-26 13:27:22 +0300

    Перевод проекта на Framework 3.5; extension-метод Join для IEnumerable<string>

---
+++ IFluent/Движок/EnumerableExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IFluent
+{
+    public static class EnumerableExt
+    {
+        public static string Join(this IEnumerable<string> enumerable, string separator = "")
+        {
+            return string.Join(separator, enumerable.ToArray());
+        }
+    }
+}
---
+++ IFluent/Движок/Типы/Логика.cs
@@ -0,0 +1,27 @@
+﻿namespace IFluent.Движок.Типы
+{
+    internal class Логика
+    {
+        private readonly bool _bool;
+
+        private Логика(bool @bool)
+        {
+            _bool = @bool;
+        }
+
+        public static implicit operator bool(Логика логика)
+        {
+            return логика._bool;
+        }
+
+        public static implicit operator Логика(bool @bool)
+        {
+            return new Логика(@bool);
+        }
+
+        public override string ToString()
+        {
+            return _bool.ToString();
+        }
+    }
+}
